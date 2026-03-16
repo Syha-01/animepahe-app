1020e6f43c7931a66035dfaf605d413503873fa0
Enrisen Tzib <enrisentzib@gmail.com>
2026-03-15 18:21:35 -0600

    chore: update API URL for production build

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://192.168.31.173:3000';
+export const API_BASE_URL = 'https://animepahe-api-2-264812564939.us-central1.run.app';
 
 export const API_ENDPOINTS = {
     airing: '/api/airing',
